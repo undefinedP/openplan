4134f7c88b600b2eb6a7962e2a762b56f3bcd6ea
undefinedP <kzbaram@gmail.com>
2025-03-26 17:40:49 +0900

    feat : reset storybook

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -11,8 +11,8 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     getAbsolutePath('@storybook/addon-essentials'),
