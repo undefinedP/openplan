803c35bba1b14a22fba18061815d10279b9172c1
undefinedP <kzbaram@gmail.com>
2025-03-28 13:15:14 +0900

    fix : vercel.json

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
+      <div className="text-5xl font-extrabold">404</div>
+      <div className="text-2xl font-bold">Looks like you&apos;re lost!</div>
+    </div>
+  );
+}
